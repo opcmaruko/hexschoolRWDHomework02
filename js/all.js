e442370a111c0e85e9003c3b2f08feba3739471f
Maruko <opcmaruko@gmail.com>
2017-07-28 12:28:41 -0500

    Add files via upload

--- js/all.js
+++ js/all.js
@@ -11,3 +11,44 @@ $(document).ready(function() {
         $('body').toggleClass('menushow');
     });
 });
+
+
+$(document).ready(function() {
+    $('.scrollTop').click(function(e) {
+        e.preventDefault();
+        var target = $(this).attr('href');
+        var targetPos = $(target).offset().top;
+        $('body').animate({ scrollTop: targetPos - 56 }, 1000);
+
+    });
+
+
+
+
+    $(window).scroll(function() {
+        var scrollPos = $(window).scrollTop();
+        var windowHeight = $(window).height();
+
+        $('.scrollTop').each(function() {
+            var target = $(this).attr('href');
+            var targetPos = $(target).offset().top;
+            var targetHeight = $(target).outerHeight();
+            if (targetPos - 57 <= scrollPos && (targetPos - 56 + targetHeight) > scrollPos) {
+                $('.scrollTop').removeClass('active')
+                $(this).addClass('active');
+            } else {
+                $(this).removeClass('active')
+            }
+        });
+        $('.animated').each(function() {
+            var thisPos = $(this).offset().top;
+            if (thisPos <= (scrollPos + windowHeight)) {
+                $(this).addClass('fadeIn');
+            }
+
+
+        });
+    });
+
+
+})
